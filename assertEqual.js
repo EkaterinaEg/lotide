30e0fc27fffd8d4b9be2f0bdea5c5bd040fc8883
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-16 21:13:04 -0400

    fix mistakes

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`😆😃😃Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,9 +7,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual(1, 2));
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
 // TEST CODE
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
