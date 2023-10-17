4076f425ae98291f3ca6693b91ab4332762f8451
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-17 09:33:18 -0400

    add test cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`😆😃😃Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,7 +7,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
@@ -22,3 +22,7 @@ const eqArrays = function(arr1, arr2) {
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([], []), true); // both arrays are empty
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // arrays have different lengths
+assertEqual(eqArrays(["test", "123"], ["test", "123"]), true); // arrays with string elements
