30febb41a554b8a081a9ae0e4a337532d32f4f67
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-16 22:22:03 -0400

    add new function tail

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😆😃😃Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (arr) {
+  const tailArray = arr.slice(1);
+  return tailArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
