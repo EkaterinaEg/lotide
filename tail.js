fa33fce6a89baa0efb00b4b4a991a751c8662090
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-16 22:30:39 -0400

    add test cases

--- tail.js
+++ tail.js
@@ -15,3 +15,10 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const emptyArray = tail([]);
+assertEqual(emptyArray.length, 0);
+const singleElementArray = tail(["Hello"]);
+assertEqual(singleElementArray.length, 0);
+const words = ["Hello", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
