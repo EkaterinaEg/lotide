a2a2fec89d029fa0121da771945ecc8dde28e1a5
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-18 09:45:08 -0400

    change approach to function without

--- without.js
+++ without.js
@@ -1,5 +1,11 @@
 const without = function(source, itemsToRemove) {
-  return source.filter((el) => !itemsToRemove.includes(el));
+  const newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
@@ -25,9 +31,13 @@ const eqArrays = function(arr1, arr2) {
 
 without([1, 2, 3], [1]); // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
 
 // test cases to check that initial arrays is not changed
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => should PASS
