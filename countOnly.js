826ad028f328ac07e840d12557b2dbdd8196e7ed
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-18 16:58:06 -0400

    implement countOnly function

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😆😃😃Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😢😢😢Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 2);
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const results = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(results["Jason"], 1);
+assertEqual(results["Karima"], undefined);
+assertEqual(results["Fang"], 2);
