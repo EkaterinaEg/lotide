aeda5ff16b77b66230edd0b2bfcc17d96800d03c
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-18 21:13:41 -0400

    correct some issues when value of object is Array

--- eqObjects.js
+++ eqObjects.js
@@ -30,7 +30,10 @@ const eqObjects = function(object1, object2) {
   }
   for (let key in object1) {
     if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+
     } else {
       if (object1[key] !== object2[key]) {
         return false;
