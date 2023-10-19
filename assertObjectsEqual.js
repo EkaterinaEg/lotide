400bf440c48d2e82dd870b3db5e7c794d1607804
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-19 09:58:34 -0400

    correct eqObject function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -29,7 +29,7 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (let key in object1) {
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
@@ -48,7 +48,7 @@ const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 
-assertObjectsEqual(shirtObject, anotherShirtObject);
-assertObjectsEqual(shirtObject, longSleeveMultiColorShirtObject);
+assertObjectsEqual(shirtObject, anotherShirtObject); //PASS
+assertObjectsEqual(shirtObject, longSleeveMultiColorShirtObject);//FaIL
 
 
--- eqObjects.js
+++ eqObjects.js
@@ -29,7 +29,7 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (let key in object1) {
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
