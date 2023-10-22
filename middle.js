ae14e968693bd5840c79abe52f50ee53a1912676
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-22 12:44:45 -0400

    add tests for middle.js function in middleTest file

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const assertArraysEqual = function(arr1, arr2) {
-  if (!eqArrays(arr1, arr2)) {
-    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
-    return;
-  }
-  console.log(`🟢🟢🟢Assertion Passed: ${arr1} === ${arr2}`);
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 const middle = function(array) {
   let middle = Math.floor(array.length / 2);
   // console.log(middle);
@@ -31,6 +10,8 @@ const middle = function(array) {
   }
 };
 
+module.exports = middle;
+
 // console.log(middle([1])); // => []
 // console.log(middle([1, 2])); // => []);
 
@@ -41,10 +22,3 @@ const middle = function(array) {
 // console.log(middle([1, 2, 3, 4])); // => [2, 3]
 // console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 // console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [4, 5]
-
-assertArraysEqual(middle([1, 2, 3]), [1, 2]); // => should FAIL
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => should PASS
-assertArraysEqual(middle([1]), []); // => should PASS
-assertArraysEqual(middle([1, 2]), []); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]); // => should PASS
